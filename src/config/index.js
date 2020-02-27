3af19ba7e18a1c9886d59491b9886836657f3efb
lizehongss <13600484164@163.com>
2020-02-27 15:46:03 +0800

    删除无用文件

--- src/config/index.js
+++ src/config/index.js
@@ -17,8 +17,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://localhost:8000/api/',
-    pro: 'https://produce.com'
+    dev: 'http://112.74.163.75:8000/api/',
+    pro: 'admin/'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
